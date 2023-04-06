f0591074b411a4e017057e2b3e5640e09d8c11eb
Yash <chandakyu20.comp@coep.ac.in>
2023-04-06 13:11:43 +0530

    ..

--- thread2.c
+++ thread2.c
@@ -12,25 +12,32 @@ extern ucontext_t main_ctx;
 int no = 0;
 
 
-void wrapper(int (*fn) (void *), void *arg) { //?
+void wrapper(int (*fn) (void *), void *arg) { 
   int ret = fn(arg); 
 }
 
-myth_t thread_create(myth_t *thread, int (*fn) (void *), void *arg) {
+myth_t thread_create_main(int (*fn) (void *), void *arg) {
   int tid;
   char *stack = malloc(4096);
   tid = clone(fn, stack + 4096, 0, arg);
-  *thread = tid;
-  printf("thread id = %d\n", *thread);
+  // *thread = tid;
+  // printf("thread id = %d\n", *thread);
 
   getcontext(&allthread[no].context);
   allthread[no].context.uc_stack.ss_sp = stack;
   allthread[no].context.uc_stack.ss_size = 4096;
   allthread[no].context.uc_link = &main_ctx;
-  makecontext(&allthread[no].context, (void (*) (void)) wrapper, 2, fn, arg); //?
+  // makecontext(&allthread[no].context, (void (*) (void)) wrapper, 2, fn, arg); //?
 
   allthread[no].tid = tid;
   no++;
+  return tid;
+}
+
+myth_t thread_create(myth_t *thread, int (*fn) (void *), void *arg) {
+  int tid = thread_create_main(fn, arg);
+  *thread = tid;
+  printf("thread id = %d\n", *thread);
   return tid;
 }
 
@@ -51,13 +58,13 @@ void thread_exit() {
   }
   ucontext_t temp = allthread[index].context;
   if (index != -1) {
-    free(allthread[index].context.uc_stack.ss_sp);
+  //   free(allthread[index].context.uc_stack.ss_sp);
 
     for (int i = index; i < no - 1; i++) {
       allthread[i] = allthread[i+1];
     }
     no--;
-    setcontext(&main_ctx); //why?
+    setcontext(&temp); //?
   }
   else {
     exit(0); //?
@@ -78,7 +85,7 @@ int thread_kill(int tid, int signal) {
   }
   //printf("%d %d %d\n",tid,tgid,ntid);
   int tgid = tid; // for one one 
-  int ret = tgkill(tgid, tid, signal);
+  int ret = tgkill(tgid, tid, signal); //thread group
   
   if (ret == -1) {
     perror("tgkill failed");
