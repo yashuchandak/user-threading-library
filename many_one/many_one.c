9d2aa06cfa1e0bb56e5900d35fcef34abea0ede1
Yash <chandakyu20.comp@coep.ac.in>
2023-04-09 22:50:50 +0530

    ..

--- many_one/many_one.c
+++ many_one/many_one.c
@@ -1,23 +1,25 @@
 #include "many_one.h"
 
-void end_timer();
 int tid = 0;
 int isFirst = 0;
-myth_Node *head = NULL;
-myth_Node *temp = NULL;
-myth_Node *curr = NULL;
-myth_Node *tail = NULL;
+
+myth_Node * head = NULL;
+myth_Node * temp = NULL;
+myth_Node * curr = NULL;
+myth_Node * tail = NULL;
 
 ucontext_t sch_ctx;
-
+extern ucontext_t main_ctx;
 struct itimerval timer;
+
+int i = 0;
 
 void begin_timer()
 {
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 100; // 0.5 millisecs
+    timer.it_value.tv_usec = 500; // 0.5 millisecs
     timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 100;
+    timer.it_interval.tv_usec = 500;
     setitimer(ITIMER_REAL, &timer, NULL);
     // printf("%d\n", timer.it_value.tv_usec);
 }
@@ -59,7 +61,7 @@ void append(myth_Node * Node){
     {
         head = Node;
         tail = Node;
-        Node->next = Node;
+        Node ->next = Node;
         temp = head;
     }
     else
@@ -74,13 +76,12 @@ void append(myth_Node * Node){
 
 int thread_create(myth_t *thread, void *(*fn) (void *), void *args)
 {
-    
     signal(SIGALRM, sig_alarm_handler);
-    myth_Node * new = allocNode(thread, fn, args);
-    getcontext(&(new->context)); //correct hmm?
+    myth_Node * new = allocNode(thread,fn,args);
+    getcontext(&(new->context)); //correct?
     new->context.uc_stack.ss_sp = new->stack;
     new->context.uc_stack.ss_size = 4096;
-    new->context.uc_link = &sch_ctx; 
+    new->context.uc_link = &sch_ctx; //?
     makecontext(&(new->context), (void(*)())fn, 1, args);
     
     append(new);
@@ -88,59 +89,162 @@ int thread_create(myth_t *thread, void *(*fn) (void *), void *args)
     {
         isFirst = 1;
         void * sch_stack = malloc(4096);
-        clone(scheduler, sch_stack+4096, CLONE_VM, NULL);
+        clone(scheduler,sch_stack+4096,CLONE_VM,NULL);
     }
+    return 0;
 }
 
-myth_Node * checkRunable ()
+int isAllTerminated()
 {
+    myth_Node * temp = head;
+    do
+    {
+        if(temp->status!=-1)
+            return 0;
+        temp = temp->next;
+    } while (temp!=head);
+    return 1;
+}
+
+myth_Node * checkRunable() {
     curr = temp;
-    do{
-        if(curr->status == 1){
+    do {
+        if (curr->status == 1) {
             temp = curr->next;
             return curr;
         }
         curr = curr->next;
-    }while(temp != curr);
-    
+    } while (temp != curr);
     return NULL;
 }
 
 int scheduler()
 {
-    
+    signal(SIGALRM, sig_alarm_handler);
     getcontext(&sch_ctx);
     while(1)
     {
-        printf("In scheduler\n");
+        printf("In sched %d\n", i);
         myth_Node * tnode = checkRunable();
-        
-        if(!tnode)
+        if(isAllTerminated())
         {
-            
+            printf("Exit\n");
+            exit(0);
         }
         else
         {
+            printf("elseme\n");
             begin_timer();
             swapcontext(&sch_ctx, &tnode->context); //timer int //1st isme current ctx save kardenga
         }
+        // printf("before sig handler\n"); //sahi
     }
+    return 0;
 }
 
 void thread_exit()
-{
+{   
+    printf("2 in exit\n");
     end_timer();
     curr->status = -1;
 }
 
-int thread_join(myth_t thread) {
-  int ret = waitpid(thread, NULL, __WALL); // wait for any child 
-  return 0;
+myth_Node * findNodeTid(int tid)
+{
+    myth_Node * t = head;
+    do
+    {
+        if(t->tid == tid)
+            // printf()
+            return t;
+        t = t->next;
+    } while (t!=head);
+    return NULL;
 }
 
-int thread_kill(int tid, int signal) {
-  
-  
-  
-  return 0;
+void handleSignal(myth_Node * curt, int signal)
+{
+    if(signal == SIGTERM || signal == SIGKILL)
+    {
+        curt->status = -1;
+        if(curt == curr) {
+            // end_timer();
+            printf("tk here\n");
+            swapcontext(&curt->context, &sch_ctx);
+        }
+    }
+    else if(signal == SIGCONT)
+    {
+        curt->status = 1;
+    }
+    else if(signal == SIGSTOP)
+    {
+        // printf("sigstop\n");
+        curt->status = 2;  //sigstop
+        if(curt == curr)   //schedule next
+        {   
+            // end_timer();
+            swapcontext(&curt->context, &sch_ctx);
+        }
+    }
 }
+
+int thread_kill(int tid, int signal)
+{   
+    
+    if( signal==SIGTERM || signal==SIGKILL || signal==SIGCONT || signal==SIGSTOP)
+    {  
+        if(!curr) {
+            // printf("df\n");
+            return -1;
+        }
+        if(curr->tid == tid)
+        {   
+            end_timer();
+            handleSignal(curr, signal);
+        }
+        else
+        {
+            myth_Node * sig_node =  findNodeTid(tid);
+            if(!sig_node) {
+                printf("sig_node not found\n");
+                return -1;
+            }
+            end_timer();
+            handleSignal(sig_node, signal);
+        }
+        return 0;
+    }
+    // printf("Dffv\n");
+    return -1;
+}
+
+int thread_join(int tid) {
+    // int ret = waitpid(thread, NULL, __WALL); // wait for any child 
+    // 
+    
+    int ret = syscall(SYS_futex, &tid, FUTEX_WAIT, tid, NULL, NULL, 0);
+
+    // myth_Node *tp = head;
+    // do {
+    //     if(tp->next->tid == tid && tp->next->status == -1) {
+    //         tp->next = tp->next->next;
+    //         if(tp->next == head) {
+    //             head = head->next;
+    //         }
+    //         if(tp->next == tail) {
+    //             tail = tp;
+    //         }
+    //         if(tp->next == tp) {
+    //             head = tail = NULL;
+    //         }
+    //         free(tp->next);
+    //         break;    
+    //     } 
+    //     tp = tp->next;    
+    // }
+    // while (tp->next != head);
+
+    return ret;
+}
+
--- many_one/test.c
+++ many_one/test.c
@@ -13,50 +13,32 @@ int x = 20;
 
 void *f1(void *arg) {
     
-    int p = *(int *)arg;
-    // printf("%d x= %d\n", p, x);
-    
-    // while(1) {    
-    // }
     printf("thread1 running\n");
 
+    for(int i=0; i< 10000; i++) {
+        printf("%d\n", i);
+    }
 
-    
     printf("thread1 exiting\n");
 
     thread_exit();
 }
 
 void *f2(void *arg) {
-    int p = *(int *)arg;
-    // printf("%d x= %d\n", p, x);
-    
+   
     printf("thread2 running\n");
-
-    // printf("First of thread2\n");
-    // sleep(0.10); //100 milliseconds
-    // for(int i=0; i<1000000; i++) {
-    //     printf("%d\n", i);
-    // }
-    // alarm(0);
-    // printf("second of thread2\n");
-    
+   
     printf("thread2 exiting\n");
 
     thread_exit();
 }
 
 void *f3(void *arg) {
-    int p = *(int *)arg;
-    // printf("%d x= %d\n", p, x);
-    
     printf("thread3 running\n");
 
-    for(int i=0; i<50000; i++) {
-        if(i%10 == 0) {
-            printf("3 %d\n", i);
-        }
-    }
+    // for(int i=0; i<100000; i++) {
+    //     printf("3 %d\n", i);
+    // }
 
     printf("thread3 exiting\n");
 
@@ -64,16 +46,11 @@ void *f3(void *arg) {
 }
 
 void *f4(void *arg) {
-    int p = *(int *)arg;
-    // printf("%d x= %d\n", p, x);
-    
     printf("thread4 running\n");
 
-    for(int i=0; i<30000; i++) {
-        if(i%10 == 0) {
-            printf("4 %d\n", i);
-        }
-    }
+    // for(int i=0; i<100000; i++) {
+    //     printf("4 %d\n", i);
+    // }
 
     printf("thread4 exiting\n");
 
@@ -81,14 +58,7 @@ void *f4(void *arg) {
 }
 
 void *f5(void *arg) {
-    int p = *(int *)arg;
-    // printf("%d x= %d\n", p, x);
-    
     printf("thread5 running\n");
-
-    // for(int i=0; i<100000; i++) {
-        
-    // }
 
     printf("thread5 exiting\n");
 
@@ -98,21 +68,30 @@ void *f5(void *arg) {
 
 
 int main() {
-
-    signal(SIGALRM, sig_alarm_handler);
-
-    getcontext(&main_ctx);
+    
     myth_t tid1, tid2, tid3, tid4, tid5;
     int arg1 = 10, arg2 = 20;
 
     thread_create(&tid1, f1, &arg1);
     thread_create(&tid2, f2, &arg2);
+    printf("tid1%dtid1\n", tid1);
+
+    for(int j=0; j<100000; j++) {
+        
+    }
+
+    // printf("dfdfv\n");
+    printf("%daaaas\n", thread_kill(0, SIGKILL));
+
     thread_create(&tid3, f3, &arg1);
     thread_create(&tid4, f4, &arg2);
     thread_create(&tid5, f5, &arg1);
     // sleep(1); 
     
+    // printf("%d\n", 
+    // thread_kill(tid1, SIGTERM);
     
+
     thread_join(tid1);
     thread_join(tid2);
     thread_join(tid3);
@@ -121,4 +100,3 @@ int main() {
 
     return 0;
 }
-
